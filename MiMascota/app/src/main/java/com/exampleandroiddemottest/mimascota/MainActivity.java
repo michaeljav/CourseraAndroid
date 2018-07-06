a172f060b08161f2f8ec01871b455ca989745836
mjavier <michaeljaviermota@gmail.com>
2018-07-06 15:43:20 -0400

    Refresh Indicator

--- MiMascota/app/src/main/java/com/exampleandroiddemottest/mimascota/MainActivity.java
+++ MiMascota/app/src/main/java/com/exampleandroiddemottest/mimascota/MainActivity.java
@@ -2,20 +2,55 @@ package com.exampleandroiddemottest.mimascota;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMilista;
+    Adapter adaptador;
+
+    int secRefres = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        sfiMiIndicadorRefresh = findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMilista = findViewById(R.id.lstMilista);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMilista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+             refrescandoContenido();
+            }
+        });
+
+
+
+    }
+
+    public void refrescandoContenido(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+
+        planetas[0]=String.valueOf(secRefres);
+        lstMilista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+
+        sfiMiIndicadorRefresh.setRefreshing(false);
+        secRefres++;
     }
 
     public void agregarFAB(){
